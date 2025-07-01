e59bd8b4acb992e24ea43c885e3082eb5e5f409e
alessandraciccone <alessandraciccone@icloud.com>
2025-07-01 17:39:44 +0200

    .

---
+++ travel.js
@@ -0,0 +1,5 @@
+const travel = function () {
+  const contaViaggio = document.querySelectorAll(".viaggio");
+  const numViaggio = contaViaggio.length;
+  alert("Ci sono " + numViaggio + " viaggi su questo sito");
+};
